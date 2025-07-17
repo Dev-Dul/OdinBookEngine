5eeb713dfd9d5d0e604f7ec360201762c1b85277
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2025-07-17 12:05:19 +0100

    chore: set up routers

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ require('dotenv').config();
 const express = require("express");
 const session = require("express-session");
 const { passport } = require("./auth/passport-config");
+const postRouter = require("./routes/postRouter");
+const gatesRouter = require("./routes/gatesRouter");
+const profileRouter = require("./routes/profileRouter");
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/api/v1/", gatesRouter);
+app.use("/api/v1/posts/", postRouter);
+app.use("/api/v1/profiles/", profileRouter);
+
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
 
 
 
--- routes/gatesRouter.js
+++ routes/gatesRouter.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
 const gatesRouter = Router();
 const gatesController = require("../controllers/gatesController");
+const { handleLogin } = require("../auth/passport-config");
 
+// get routes
 gatesRouter.get("/:userId", gatesController.getUserById);
 gatesRouter.get("/user/:userName", gatesController.getUserByusername);
+
+// post routes
+gatesRouter.post("/login", handleLogin);
 gatesRouter.post("/signup", gatesController.createNewUserLocal);
 
 module.exports = gatesRouter;
